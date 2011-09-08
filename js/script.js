e7f26c6a2d9d3291ab8a4127cf7b862c03533be0
Stu Rob <stumrob@gmail.com>
2011-09-08 20:25:25 +0100

    tweaks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(function(){
-
-});
+  
+  
+  if (window.location.search == '?vip=added') {
+    $('body').addClass('subbed');
+    $('#main').click(function() {
+      window.location.search = '';
+    })
+  }
+});

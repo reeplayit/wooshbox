4da12de2e3b5caf4ec3ddb423451977bf096a8b4
Stu Rob <stumrob@gmail.com>
2011-09-08 22:35:06 +0100

    doh

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  if (window.location.hash == '#addded') {
+  if (window.location.hash == '#added') {
     $('body').addClass('subbed');
     $('#main').click(function() {
       window.location.search = '';

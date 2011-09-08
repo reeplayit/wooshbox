607a534a824ab938501e562e166309ef8d03c1f5
Stu Rob <stumrob@gmail.com>
2011-09-08 22:34:15 +0100

    fix bookmarking/sharing a confirmation url bug

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(function(){
-  
-  
-  if (window.location.search == '?vip=added') {
+
+  if (window.location.hash == '#addded') {
     $('body').addClass('subbed');
     $('#main').click(function() {
       window.location.search = '';
-    })
+    });
+    window.location.hash = '';
   }
 });

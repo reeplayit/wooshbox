9ad517ca8e420c80d52f4b3ebfd234d32f89c20d
Stu Rob <stumrob@gmail.com>
2011-09-09 13:48:38 +0100

    signed up event for kiss

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(function(){
 
   if (added) {
+    _kmq.push(['record', 'Signed Up']);
+    
     $('body').addClass('subbed');
     $('#main').click(function() {
       window.location.search = '';
